002141456b8f80d6c366747897634aed454b8fd7
kiego <55186979+kiego@users.noreply.github.com>
2021-06-28 18:24:28 +0900

    Default Changelist

--- bloomfilter.py
+++ bloomfilter.py
@@ -3,21 +3,21 @@ import glob
 
 
 
-bfindex = bytearray([0] * 64)
-file_data = []
-
-def hash_value(keys,flag):
+def hash_value(keys, bfindex, flag):
     for key_value in keys:
         md5 = int(hashlib.md5(key_value).hexdigest(),16) % 64
         sha256 = int(hashlib.sha256(key_value).hexdigest(),16)% 64
         sha512 = int(hashlib.sha512(key_value).hexdigest(),16) % 64
         
-        if flag == True:
+        if flag:
             bfindex[md5] = 1
             bfindex[sha256] = 1
             bfindex[sha512] = 1
-        elif flag == False:
-            serch(md5,sha256,sha512)
+        else:
+            search(md5, sha256, sha512)
+
+    return bfindex
+
 
 def check_file_count():
     # ファイル一覧取得
@@ -33,18 +33,36 @@ def input_value(file_path):
             file_data.append(f.read())
         return file_data
 
-# def serch(md5,sha256,sha512):
+def search(md5,sha256,sha512):
+    keyword_value = [md5,sha256,sha512]
+    for bit in keyword_value:
+        if bfindex[bit] == 0:
+            print("検索したキーワードはありません")
+            break
+        else:
+            print("aru")
+            continue
 
 
+def input_keyword():
+    key = input("enter your keyword: ")
+    word_list = key.split(" ")
+    keyword_list = [x.encode() for x in word_list]
 
-if __name__ == '__main__':
+    return keyword_list
 
 
+if __name__ == "__main__":
+    bfindex = bytearray([0] * 64)
+    file_data = []
     flag = True
+
     file_path = check_file_count()
-    keys = input_value(file_path,flag)
-    hash_value(keys)
-    keyword = input("enter your keyword: ")
+    keys = input_value(file_path)
+    bfindex = hash_value(keys, bfindex, flag)
+    keyword = input_keyword()
+    flag = False
+    hash_value(keyword, bfindex, flag)
 
 
     
--- import hashlib.py
+++ import hashlib.py
@@ -1,3 +1,8 @@
-f = open('myfile.dat', 'wb')
-f.write(b'ABCDEFG')
-f.close()
+m = 8
+k = 4
+n = 20
+
+
+v = (1 - ((1-1/m)**(n*k)))**k
+
+print(v)
